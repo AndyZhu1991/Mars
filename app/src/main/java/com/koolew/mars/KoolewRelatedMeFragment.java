ef8466d131af66814da37a83fc3eb126cba60adf
jinchang.zhu <jinchangzhu@jinchangzhus-MacBook-Pro.local>
2015-06-25 20:42:15 +0800

    Add koolew related me fragment(UI only, use fake data).

--- app/src/main/java/com/koolew/mars/KoolewRelatedMeFragment.java
+++ app/src/main/java/com/koolew/mars/KoolewRelatedMeFragment.java
@@ -1,13 +1,24 @@
 package com.koolew.mars;
 
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class KoolewRelatedMeFragment extends Fragment {
+
+    private ListView mListView;
+    private RelatedMeAdapter mAdapter;
 
     /**
      * Use this factory method to create a new instance of
@@ -34,7 +45,85 @@ public class KoolewRelatedMeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_koolew_related_me, container, false);
+        View root = inflater.inflate(R.layout.fragment_koolew_related_me, container, false);
+
+        mListView = (ListView) root.findViewById(R.id.list_view);
+        mAdapter = new RelatedMeAdapter();
+        generateTestData(mAdapter.mData);
+        mListView.setAdapter(mAdapter);
+
+        return root;
     }
 
+    private void generateTestData(List<RelatedMeItem> list) {
+        for (int i = 0; i < 100; i++) {
+            list.add(new RelatedMeItem("title " + i, i));
+        }
+    }
+
+    class RelatedMeItem {
+        String title;
+        int videoCount;
+
+        RelatedMeItem(String title, int videoCount) {
+            this.title = title;
+            this.videoCount = videoCount;
+        }
+    }
+
+    private static final int[] LEFT_LAYOUT_COLORS = {
+            0xFFFF5656, 0xFFFC7B7B, 0xFFFFAE82, 0xFFFFC282,
+            0xFFF4D288, 0xFFFFC282, 0xFFFFAE82, 0xFFFC7B7B,
+    };
+
+    class RelatedMeAdapter extends BaseAdapter {
+
+        List<RelatedMeItem> mData;
+
+        RelatedMeAdapter() {
+            mData = new LinkedList<RelatedMeItem>();
+        }
+
+        @Override
+        public int getCount() {
+            return mData.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mData.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getActivity())
+                        .inflate(R.layout.koolew_relative_me_item, null);
+                ViewHolder holder = new ViewHolder();
+                holder.leftLayout = (LinearLayout) convertView.findViewById(R.id.left_layout);
+                holder.videoCount = (TextView) convertView.findViewById(R.id.video_count);
+                holder.title = (TextView) convertView.findViewById(R.id.title);
+                convertView.setTag(holder);
+            }
+
+            ViewHolder holder = (ViewHolder) convertView.getTag();
+            ((GradientDrawable) holder.leftLayout.getBackground())
+                    .setColor(LEFT_LAYOUT_COLORS[position % 8]);
+            holder.videoCount.setText("" + mData.get(position).videoCount);
+            holder.title.setText(mData.get(position).title);
+
+            return convertView;
+        }
+    }
+
+    class ViewHolder {
+        LinearLayout leftLayout;
+        TextView videoCount;
+        TextView title;
+    }
 }
