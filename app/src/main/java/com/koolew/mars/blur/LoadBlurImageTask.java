f21ff7feca9dd9354089af8a2404d688ed489dfa
jinchang.zhu <jinchangzhu@jinchangzhus-MacBook-Pro.local>
2015-07-01 19:48:48 +0800

    Fix a FC bug: Bitmap zero-size

--- app/src/main/java/com/koolew/mars/blur/LoadBlurImageTask.java
+++ app/src/main/java/com/koolew/mars/blur/LoadBlurImageTask.java
@@ -28,8 +28,24 @@ public class LoadBlurImageTask extends AsyncTask {
     protected Object doInBackground(Object[] params) {
         Bitmap bmp = ImageLoader.getInstance().loadImageSync(mUri);
 
-        int viewWidth = mView.getMeasuredWidth();
-        int viewHeight = mView.getMeasuredHeight();
+        int viewWidth = 0;
+        int viewHeight = 0;
+
+        // Wait 1 second for measure
+        for (int i = 0; i < 10; i++) {
+            viewWidth = mView.getMeasuredWidth();
+            viewHeight = mView.getMeasuredHeight();
+            if (viewWidth == 0 || viewHeight == 0) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            else {
+                break;
+            }
+        }
 
         Bitmap blurBitmap = BitmapUtil.getClipedScaledBitmap(bmp,
                 (int) (viewWidth / SCALE_BEFORE_BLUR), (int) (viewHeight / SCALE_BEFORE_BLUR));
