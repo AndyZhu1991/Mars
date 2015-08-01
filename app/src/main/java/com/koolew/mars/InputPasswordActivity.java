b944752fd958279aff513c421f88c84dfcb9403a
jinchang.zhu <jinchangzhu@jinchangzhus-MacBook-Pro.local>
2015-08-01 15:58:50 +0800

    Add call password

--- app/src/main/java/com/koolew/mars/InputPasswordActivity.java
+++ app/src/main/java/com/koolew/mars/InputPasswordActivity.java
@@ -1,7 +1,6 @@
 package com.koolew.mars;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,10 +9,8 @@ import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.Request.Method;
 import com.android.volley.RequestQueue;
@@ -38,19 +35,18 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 
-public class InputPasswordActivity extends Activity implements View.OnClickListener{
+public class InputPasswordActivity extends Activity implements View.OnClickListener,
+        RequestPasswordFragment.OnFragmentInteractionListener{
 
     private static final String TAG = "koolew-InputPasswordA";
 
     private EditText mPasswordCapture;
     private TextView[] mPasswordDigits = new TextView[4];
     private TextView mHintPhoneNumber;
-    private Button mResendPassword;
-    private ProgressDialog mRequestingDialog;
+
+    private RequestPasswordFragment mFragment;
 
     private RequestQueue mRequestQueue;
-    private int resendPasswordCountDown;
-    private Timer mCountDownTimer = new Timer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,9 +57,12 @@ public class InputPasswordActivity extends Activity implements View.OnClickListe
         mPasswordCapture = (EditText) findViewById(R.id.password_capture);
         mPasswordCapture.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
 
             @Override
             public void afterTextChanged(Editable s) {
@@ -71,22 +70,20 @@ public class InputPasswordActivity extends Activity implements View.OnClickListe
                 updatePasswordDigits(s.toString());
             }
         });
-        mRequestingDialog = new ProgressDialog(this);
-        mRequestingDialog.setMessage(getString(R.string.requesting_password));
-        mRequestingDialog.setCanceledOnTouchOutside(false);
-        mRequestingDialog.setIndeterminate(true);
         int[] passwordDigitIds = { R.id.password_digit1, R.id.password_digit2,
                                    R.id.password_digit3, R.id.password_digit4};
         for (int i = 0; i < 4; i++) {
-            mPasswordDigits[i] = (TextView) findViewById(passwordDigitIds[i]).findViewById(R.id.digit_text);
+            mPasswordDigits[i] = (TextView)
+                    findViewById(passwordDigitIds[i]).findViewById(R.id.digit_text);
             mPasswordDigits[i].setOnClickListener(this);
         }
 
         mHintPhoneNumber = (TextView) findViewById(R.id.hint_phone_number);
         mHintPhoneNumber.setText(MyAccountInfo.getPhoneNumber());
-        mResendPassword = (Button) findViewById(R.id.btn_resend_password);
 
-        requestPassword();
+        mFragment = (RequestPasswordFragment) getFragmentManager().
+                findFragmentById(R.id.request_password_fragment);
+        mFragment.sendMessage();
     }
 
     @Override
@@ -112,87 +109,6 @@ public class InputPasswordActivity extends Activity implements View.OnClickListe
         }, delay);
     }
 
-    private void requestPassword() {
-
-        // Just 4 debug
-        //if (true) return;
-
-        String url = UrlHelper.getRequestPasswordMessageUrl(MyAccountInfo.getPhoneNumber());
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Method.GET, url,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Log.d(TAG, response.toString());
-                        try {
-                            if (response.getInt("code") == 0) {
-                                requestPasswordSuccess();
-                            }
-                            else {
-                                // Error?
-                                mRequestingDialog.dismiss();
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e("TAG", error.getMessage(), error);
-                        if (mRequestingDialog.isShowing()) {
-                            mRequestingDialog.dismiss();
-                        }
-                        Toast.makeText(InputPasswordActivity.this,
-                                R.string.connect_server_failed, Toast.LENGTH_SHORT).show();
-                    }
-        });
-        mRequestQueue.add(jsonObjectRequest);
-        mRequestingDialog.show();
-
-//        KoolewApiClient.get("user/code", new RequestParams("phone", AccountInfo.getPhoneNumber()), new JsonHttpResponseHandler() {
-//            @Override
-//            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-//                // If the response is JSONObject instead of expected JSONArray
-//                try {
-//                    Log.d(TAG, "code: " + response.getInt("code") + ", msg: " + response.getString("msg"));
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
-
-    }
-
-    private void requestPasswordSuccess() {
-        resendPasswordCountDown = 60;
-        mResendPassword.setEnabled(false);
-        mResendPassword.setText(getResources().getString(R.string.resend_password) +
-                getResources().getString(R.string.count_down_string, resendPasswordCountDown));
-        resendPasswordCountDown--;
-        mCountDownTimer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (resendPasswordCountDown != 0) {
-                            mResendPassword.setText(getResources().getString(R.string.resend_password) +
-                                    getResources().getString(R.string.count_down_string, resendPasswordCountDown));
-                        } else {
-                            mResendPassword.setText(getResources().getString(R.string.resend_password));
-                            mResendPassword.setEnabled(true);
-                            mCountDownTimer.cancel();
-                        }
-                        resendPasswordCountDown--;
-                    }
-                });
-            }
-        }, 1000, 1000);
-        mRequestingDialog.dismiss();
-        showSoftKeyInput(100);
-    }
-
     private void updatePasswordDigits(String s) {
         int length = s.length();
         if (length > 4) {
@@ -208,31 +124,6 @@ public class InputPasswordActivity extends Activity implements View.OnClickListe
     }
 
     private void onPasswordInputComplete(final String password) {
-//        RequestParams params = new RequestParams();
-//        params.put("phone", AccountInfo.getPhoneNumber());
-//        params.put("code", password);
-//        KoolewApiClient.post("user/login", params, new JsonHttpResponseHandler() {
-//            @Override
-//            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-//                // If the response is JSONObject instead of expected JSONArray
-//                try {
-//                    Log.d(TAG, "code: " + response.getInt("code") + ", msg: " + response.getString("msg"));
-//                    Iterator<String> ss = response.keys();
-//                    while(ss.hasNext()){
-//                        Log.d(TAG, "key: " + ss.next());
-//                    }
-//                    JSONObject result = response.getJSONObject("result");
-//                    Log.d(TAG, "token: " + result.getString("token") +
-//                            ", uid: "  + result.getString("uid") + "register: " + result.getInt("register"));
-//                    JSONObject info = result.getJSONObject("info");
-//                    Log.d(TAG, "uid: " + info.getString("uid") + ", avatar: " + info.getString("avatar") +
-//                            ", nickname: " + info.getString("nickname") + ", koo_num " + info.getInt("koo_num") +
-//                            ", coin_num: " + info.getInt("coin_num") + ", phone: " + info.getString("phone"));
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
 
         String url = null;
         JSONObject requestJson = new JSONObject();
@@ -332,8 +223,12 @@ public class InputPasswordActivity extends Activity implements View.OnClickListe
         }
     }
 
-    public void onResendPassword(View v) {
-        Log.d(TAG, "onResendPassword");
-        requestPassword();
+    @Override
+    public String getPhoneNumber() {
+        return MyAccountInfo.getPhoneNumber();
+    }
+
+    @Override
+    public void onPhoneNumberInvalid() {
     }
 }
