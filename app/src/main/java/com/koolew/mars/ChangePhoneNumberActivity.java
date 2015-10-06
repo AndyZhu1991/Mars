3d15128ec54c1b9c233222c53c4d2c576caa75a5
jinchang.zhu <stdzhu@gmail.com>
2015-10-06 16:04:27 +0800

    优化了修改手机号的操作流程

--- app/src/main/java/com/koolew/mars/ChangePhoneNumberActivity.java
+++ app/src/main/java/com/koolew/mars/ChangePhoneNumberActivity.java
@@ -2,7 +2,9 @@ package com.koolew.mars;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.InputType;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,7 +16,7 @@ import com.koolew.mars.infos.MyAccountInfo;
 import com.koolew.mars.statistics.BaseActivity;
 import com.koolew.mars.utils.DialogUtil;
 import com.koolew.mars.utils.Utils;
-import com.koolew.mars.view.TitleBarView;
+import com.koolew.mars.webapi.ApiErrorCode;
 import com.koolew.mars.webapi.ApiWorker;
 
 import org.json.JSONException;
@@ -22,8 +24,7 @@ import org.json.JSONObject;
 
 
 public class ChangePhoneNumberActivity extends BaseActivity
-        implements RequestPasswordFragment.OnFragmentInteractionListener,
-        TitleBarView.OnRightLayoutClickListener {
+        implements RequestPasswordFragment.OnFragmentInteractionListener {
 
     private EditText mNewNumber;
     private EditText mPasswordEdit;
@@ -42,8 +43,6 @@ public class ChangePhoneNumberActivity extends BaseActivity
     }
 
     private void initViews() {
-        ((TitleBarView) findViewById(R.id.title_bar)).setOnRightLayoutClickListener(this);
-
         ((TextView) findViewById(R.id.original_number)).setText(
                 getString(R.string.original_number_is, MyAccountInfo.getPhoneNumber()));
 
@@ -57,6 +56,7 @@ public class ChangePhoneNumberActivity extends BaseActivity
         mPasswordEdit = (EditText) editPassword.findViewById(R.id.edit_text);
         mPasswordEdit.setHint(R.string.input_password_hint);
         mPasswordEdit.setRawInputType(InputType.TYPE_CLASS_NUMBER);
+        mPasswordEdit.addTextChangedListener(new PasswordLengthWatcher());
         editPassword.findViewById(R.id.x).setOnClickListener(new OnPasswordClearListener());
     }
 
@@ -122,12 +122,28 @@ public class ChangePhoneNumberActivity extends BaseActivity
     public void onPhoneNumberInvalid() {
     }
 
-    @Override
-    public void onRightLayoutClick() {
-        // TODO: On 'save' click
+    private void onPasswordInputComplete() {
         ApiWorker.getInstance().updatePhoneNumber(mNewNumber.getText().toString(),
                 mPasswordEdit.getText().toString(), mResponseListener, mErrorListener);
         mConnectingDialog.show();
+    }
+
+    class PasswordLengthWatcher implements TextWatcher {
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (s.length() == 4) {
+                onPasswordInputComplete();
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
     }
 
     private Response.Listener<JSONObject> mResponseListener = new Response.Listener<JSONObject>() {
@@ -142,9 +158,17 @@ public class ChangePhoneNumberActivity extends BaseActivity
                     finish();
                 }
                 else {
-                    mConnectingDialog.dismiss();
-                    Toast.makeText(ChangePhoneNumberActivity.this,
-                            R.string.failed, Toast.LENGTH_SHORT).show();
+                    if (code == ApiErrorCode.PHONE_CODE_ERROR) {
+                        mConnectingDialog.dismiss();
+                        Toast.makeText(ChangePhoneNumberActivity.this,
+                                R.string.error_phone_code, Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        mConnectingDialog.dismiss();
+                        Toast.makeText(ChangePhoneNumberActivity.this,
+                                R.string.failed, Toast.LENGTH_SHORT).show();
+                    }
+                    mPasswordEdit.setText("");
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
